0ae6f8bb6fdcf244c0453fb74087b11095b6163d
Scott Robertson <contact@ScottRobertson.io>
2019-08-15 00:07:47 -0500

    ngx-bootstrap and fontawesome svg

--- projects/my-lib/src/lib/components/button-a/button-a.module.ts
+++ projects/my-lib/src/lib/components/button-a/button-a.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { ButtonAComponent } from './button-a.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
+    imports: [FontAwesomeModule],
     declarations: [ButtonAComponent],
     exports: [ButtonAComponent]
 })
