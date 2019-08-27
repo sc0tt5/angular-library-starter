ad8e1d42afbe6fdd29eecddb7f20fe95a3143401
Scott Robertson <contact@ScottRobertson.io>
2019-08-26 21:29:23 -0500

    :pizza: is yummy

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { AppNavComponent } from './app-nav/app-nav.component';
 
-// todo: header, footer, etc.
+// todo: header and footer
 @NgModule({
     imports: [CommonModule, RouterModule],
     declarations: [AppNavComponent],
