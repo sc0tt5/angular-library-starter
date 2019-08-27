e39e35dd4eec0a714c949bb75f2c46814e69017e
Scott Robertson <contact@ScottRobertson.io>
2019-08-26 21:26:14 -0500

    :refactor: todo test

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { AppNavComponent } from './app-nav/app-nav.component';
 
+// todo: header, footer, etc.
 @NgModule({
     imports: [CommonModule, RouterModule],
     declarations: [AppNavComponent],
